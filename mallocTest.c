9153ada3a91aa4f812c661611c21ed78bdf46bc3
ljp <li_jp@Ctrip.com>
2018-09-28 14:21:41 +0800

    去掉警告

--- mallocTest.c
+++ mallocTest.c
@@ -4,8 +4,8 @@
 int main(int argc, char **argv) {
     int i = 0;
     for(i = 0; i < 1000000; i++) {
-        malloc(32);
+       void * result = malloc(32);
     }
-    printf("hello world");
+    printf("hello world\n");
     exit(0);
 }
