41e509ea6cac5e7e9eb456e490a2f9f7c38e384d
ljp <li_jp@Ctrip.com>
2018-09-28 14:17:52 +0800

    add ignore

--- mallocTest.c
+++ mallocTest.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char **argv) {
     int i = 0;
     for(i = 0; i < 1000000; i++) {
         malloc(32);
     }
+    printf("hello world");
     exit(0);
 }
